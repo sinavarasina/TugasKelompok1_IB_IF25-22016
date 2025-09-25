8020eebeac72d1a215eba52ecd99b90d32620db2
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-09-25 20:50:32 +0700

    initiate flake

---
+++ uniform_cost_search/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+      pkgs.mkShell {
+        packages = with pkgs; [
+          deno
+        ];
+
+        shellHook = ''
+          exec zsh
+        '';
+      };
+  };
+}
