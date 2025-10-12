61f9542c52ec28e72e88a22d5b0bb506d4e9479b
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-10-12 20:06:57 +0700

    making simple algorithm, well, looks like this is not a backtracking mrv enough, i'm gonna change it real quick

--- Tugas_2/backtracking/main.go
+++ Tugas_2/backtracking/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	// Variabel
@@ -9,13 +12,81 @@ func main() {
 	// Domain
 	kelas := [2][]string{{}, {}}
 
-	i := true
-	for i == true {
-		// Constrains 1
-		if mahasiswa[0] == "Ani" ||  mahasiswa[0] == "Budi" {
+	// Loop
+	loop := true
+	for loop == true {
+		if len(mahasiswa) == 0 {
+			break
 		}
 
-		i = false
+		if mahasiswa[0] == "Ani" {
+			for i := range kelas {
+				if slices.Contains(kelas[i], "Budi") == false {
+					kelas[i] = append(kelas[i], "Ani")
+					mahasiswa = mahasiswa[1:]
+					break
+				}
+			}
+
+		} else if mahasiswa[0] == "Budi" {
+			for i := range kelas {
+				if slices.Contains(kelas[i], "Ani") == false {
+					kelas[i] = append(kelas[i], "Budi")
+					mahasiswa = mahasiswa[1:]
+					break
+				}
+			}
+
+		} else if mahasiswa[0] == "Citra" {
+			kelas1 := len(kelas[0])
+			kelas2 := len(kelas[1])
+
+			lowest := 0
+			if kelas2 < kelas1 {
+				lowest = 1
+			}
+			for i := range kelas {
+				if slices.Contains(kelas[i], "Dedi") == true {
+					kelas[i] = append(kelas[i], "Citra")
+					mahasiswa = mahasiswa[1:]
+					break
+				}
+			}
+			kelas[lowest] = append(kelas[lowest], "Citra")
+			mahasiswa = mahasiswa[1:]
+
+		} else if mahasiswa[0] == "Dedi" {
+			kelas1 := len(kelas[0])
+			kelas2 := len(kelas[1])
+
+			lowest := 0
+			if kelas2 < kelas1 {
+				lowest = 1
+			}
+
+			for i := range kelas {
+				if slices.Contains(kelas[i], "Citra") == true {
+					kelas[i] = append(kelas[i], "Dedi")
+					mahasiswa = mahasiswa[1:]
+					break
+				}
+			}
+			kelas[lowest] = append(kelas[lowest], "Dedi")
+			mahasiswa = mahasiswa[1:]
+
+		} else if mahasiswa[0] == "Eka" {
+			for i := range kelas {
+				if slices.Contains(kelas[i], "Citra") == true {
+					kelas[i] = append(kelas[i], "Dedi")
+					mahasiswa = mahasiswa[1:]
+					break
+				}
+			}
+			kelas[0] = append(kelas[0], "Dedi")
+			mahasiswa = mahasiswa[1:]
+
+		}
+
+		fmt.Println(kelas)
 	}
-	fmt.Println("Hello from src!")
 }
