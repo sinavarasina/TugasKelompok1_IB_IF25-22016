acd60df6323985d34bb9fdb5249ac10e622c97c9
faiq1818 <ghozyerlanggafaiq@gmail.com>
2025-11-01 02:48:38 +0700

    some refactoring

--- Tugas_3/forward_chaining/main.go
+++ Tugas_3/forward_chaining/main.go
@@ -8,7 +8,7 @@ import (
 type Rule struct {
 	If       []string
 	Then     string
-	Priority string
+	Priority int
 }
 
 func main() {
@@ -19,42 +19,79 @@ func main() {
 	}
 	_ = facts
 
+	factOrder := []string{"Mesin Mati Total", "Suara Klik Saat Start", "Tidak Ada Karat Pada Terminal"}
+
 	rules := []Rule{
-		{If: []string{"Mesin Mati Total"}, Then: "Cek Kelistrikan", Priority: "1"},
-		{If: []string{"Mesin Berputar Lambat"}, Then: "Aki Lemah", Priority: "2"},
-		{If: []string{"Lampu Redup"}, Then: "Aki Lemah", Priority: "2"},
-		{If: []string{"Aki Lemah", "Tidak Ada Karat Pada Terminal"}, Then: "Ganti Aki", Priority: "3"},
-		{If: []string{"Suara Klik Saat Start"}, Then: "Aki Lemah", Priority: "2"},
-		{If: []string{"Mesin Mati Total", "Tidak Ada Suara"}, Then: "Fungsi Kelistrikan Terputus", Priority: "3"},
-		{If: []string{"Aki Lemah"}, Then: "Mesin Sulit Start", Priority: "2"},
-		{If: []string{"Cek Kelistrikan", "Terjadi Konsleting"}, Then: "Isolasi Kelistrikan", Priority: "4"},
+		{If: []string{"Mesin Mati Total"}, Then: "Cek Kelistrikan", Priority: 1},
+		{If: []string{"Mesin Berputar Lambat"}, Then: "Aki Lemah", Priority: 2},
+		{If: []string{"Lampu Redup"}, Then: "Aki Lemah", Priority: 2},
+		{If: []string{"Aki Lemah", "Tidak Ada Karat Pada Terminal"}, Then: "Ganti Aki", Priority: 3},
+		{If: []string{"Suara Klik Saat Start"}, Then: "Aki Lemah", Priority: 2},
+		{If: []string{"Mesin Mati Total", "Tidak Ada Suara"}, Then: "Fungsi Kelistrikan Terputus", Priority: 3},
+		{If: []string{"Aki Lemah"}, Then: "Mesin Sulit Start", Priority: 2},
+		{If: []string{"Cek Kelistrikan", "Terjadi Konsleting"}, Then: "Isolasi Kelistrikan", Priority: 4},
 	}
 
-	// sort the rules descending priority (Rule Order)
+	// sort the rules descending priority (1. Rule Order)
 	sort.SliceStable(rules, func(i, j int) bool {
+		// sort the rules by specificity  (2. Specificity)
+		if rules[i].Priority == rules[j].Priority {
+			return len(rules[i].If) > len(rules[j].If)
+		}
 		return rules[i].Priority > rules[j].Priority
 	})
 
+	usedRules := make(map[int]bool)
+
 	changed := true
-	fmt.Println("facts : ", facts)
+	loop := 0
 	for changed {
+		loop++
+		fmt.Println(">>>>>>>>>>>>>>>>>>>>>>>>>>", loop, "<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 		changed = false
 
 		for i, rule := range rules {
+			// skip the rule by used rules (Refractoriness)
+			if usedRules[i] {
+				continue
+			}
+
 			satisfied := true
-			fmt.Println(i)
+			fmt.Println("R", i)
 			for _, cond := range rule.If {
-				fmt.Println("====Cond", cond)
-				fmt.Println("====priority", rule.Priority)
+				fmt.Println("Cond : ", cond)
+				fmt.Println("priority : ", rule.Priority)
+				fmt.Println(cond, "-------->", rule.Then)
 				if !facts[cond] {
 					satisfied = false
 					break
 				}
-				fmt.Println("ok")
+				fmt.Println("ok ok ok ok ok ok ok")
+				fmt.Println("------------")
 			}
+
 			if !satisfied {
-				fmt.Println("nooooo")
+				fmt.Println("xxxxxxxxxxxx")
+				fmt.Println("------------")
 			}
+
+			if satisfied && !facts[rule.Then] {
+				fmt.Println("Rule terpenuhi:", rule.Then)
+				facts[rule.Then] = true
+				factOrder = append(factOrder, rule.Then)
+				usedRules[i] = true
+				changed = true
+			}
+
+			fmt.Printf("=====Facts=====")
+			fmt.Println()
+
+			for _, f := range factOrder {
+				fmt.Printf(" - %s\n", f)
+			}
+
+			fmt.Println()
+			fmt.Println()
 		}
 	}
 
